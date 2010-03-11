74989b5f4a7b46331f99ca6a2d1b79167eb1f2bb
luelher <luelher@b3dbce82-d11d-11de-aacf-132551bdafba>
2010-03-11 16:25:58 +0000

    Clase que faltaba por subir, del formulario de insercion de cartas entregadas
    git-svn-id: http://gescob.googlecode.com/svn/trunk/gescob@43 b3dbce82-d11d-11de-aacf-132551bdafba

--- lib/form/insertarCartaForm.class.php
+++ lib/form/insertarCartaForm.class.php
@@ -5,14 +5,24 @@ class insertarCartasForm extends sfForm
   public function configure()
   {
     $this->setWidgetSchema(new sfWidgetFormSchema(array(
-      'fecha_entrega'   => new sfWidgetFormInput(array(),array('onfocus' => 'Calendar.setup({trigger : "fecha_entrega", inputField : "fecha_entrega", ifFormat : "%d/%m/%Y"});','size' => 10)),
+      'zona'  => new sfWidgetFormSelect(array('choices' => ZonaPeer::LasZonas()),array('selected' => '')) ,
+      'fecha_entrega'   => new sfWidgetFormInput(array(),array('onfocus' => 'Calendar.setup({trigger : "cartas_fecha_entrega", inputField : "cartas_fecha_entrega", ifFormat : "%d/%m/%Y"});','size' => 10)),
       'cedula'  => new sfWidgetFormInput(array(),array()) ,
+
     )));
 
     //$this->setDefault('fecha_desde', H::AddDaysDate(date('d/m/Y'), -7));
     //$this->setDefault('fecha_hasta', date('d/m/Y'));
 
     $this->widgetSchema->setFormFormatterName('list');
+
+    $this->widgetSchema->setNameFormat('cartas[%s]');
+
+    $this->setValidators(array(
+      'cedula'  => new sfValidatorNumber(array('required' => true, 'max' => 99999999, 'min' => 1000)),
+      'fecha_entrega'  => new sfValidatorDate(array('date_format' => '/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/')),
+      'zona' => new sfValidatorChoice(array('required' => true, 'choices' => array_keys(ZonaPeer::LasZonas()))),
+    ));
 
   }
 }
