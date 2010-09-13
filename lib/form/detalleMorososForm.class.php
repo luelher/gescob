2036308eaaabbeef7cebb3bb50ee6987a2a05f86
luelher <luelher@b3dbce82-d11d-11de-aacf-132551bdafba>
2010-09-13 04:28:58 +0000

    Se cambio formulario de morosos, para cargar la información del mensaje a enviar
    git-svn-id: http://gescob.googlecode.com/svn/trunk/gescob@60 b3dbce82-d11d-11de-aacf-132551bdafba

--- lib/form/detalleMorososForm.class.php
+++ lib/form/detalleMorososForm.class.php
@@ -14,9 +14,16 @@ class detalleMorososForm extends sfForm {
       'resultado'  => new sfWidgetFormGrid(array('config' => $this->getOption('config'),'per' => $per)) ,
       'cantidad_de_clientes'   => new sfWidgetFormInput(array(),array('readonly'=>true, 'value' => count($per))),
       'total_pagado'   => new sfWidgetFormInput(array(),array('readonly'=>true, 'value' => '', 'name' => 'totalp')),
+      'mensaje' => new sfWidgetFormTextarea(array(),array('rows' => '3', 'cols' => '60')),
     )));
 
     $this->widgetSchema->setFormFormatterName('list');
+
+    $opciones = OpcionesPeer::doSelectOne(new Criteria());
+    if($opciones){
+      $this->setDefault('mensaje', $opciones->getMensaje());
+    }
+    
   }
 
 }
