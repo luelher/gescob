40f1e94391bbbcf8b06b8c935cb76dd70bc383e0
luelher <luelher@b3dbce82-d11d-11de-aacf-132551bdafba>
2010-10-21 22:59:25 +0000

    Se agrego un reporte nuevo y se modifico los pagos para que muestre bien la informacion de los dias sin cancelar
    git-svn-id: http://gescob.googlecode.com/svn/trunk/gescob@69 b3dbce82-d11d-11de-aacf-132551bdafba

--- lib/form/buscarFrecuenciaPagoForm.class.php
+++ lib/form/buscarFrecuenciaPagoForm.class.php
@@ -6,17 +6,20 @@ class buscarFrecuenciaPagoForm extends sfForm
   {
     $this->setWidgetSchema(new sfWidgetFormSchema(array(
       'dias_desde'  => new sfWidgetFormInput(array(),array('size' => 10)) ,
-      'dias_hasta'   => new sfWidgetFormInput(array(),array('size' => 10)),
+      'dias_hasta'  => new sfWidgetFormInput(array(),array('size' => 10)),
+      'fecha_hasta' => new sfWidgetFormInput(array(),array('onfocus' => 'Calendar.setup({trigger : "fecha_hasta", inputField : "fecha_hasta", ifFormat : "%d/%m/%Y"});','size' => 10)),
     )));
-    
+
     $this->setDefault('dias_desde', 0);
     $this->setDefault('dias_hasta', 30);
+    $this->setDefault('fecha_hasta', date('d/m/Y'));
 
     $this->widgetSchema->setFormFormatterName('list');
 
     $this->setValidators(array(
       'dias_desde'  => new sfValidatorNumber(array('required' => true, 'max' => 1000, 'min' => 0)),
       'dias_hasta'  => new sfValidatorNumber(array('required' => true, 'max' => 1000, 'min' => 0)),
+      'fecha_hasta'  => new sfValidatorDate(array('required' => true)),
     ));
 
   }
