9cc4d50b15b88b521ae1bf5a6114e8fdee2993e0
luelher <luelher@b3dbce82-d11d-11de-aacf-132551bdafba>
2010-10-30 16:31:49 +0000

    se cambio el campo de camtidad de clientes para que no tomara en cuenta los que estan repetidos
    git-svn-id: http://gescob.googlecode.com/svn/trunk/gescob@71 b3dbce82-d11d-11de-aacf-132551bdafba

--- lib/form/detalleMorososForm.class.php
+++ lib/form/detalleMorososForm.class.php
@@ -10,9 +10,22 @@ class detalleMorososForm extends sfForm {
   public function configure($options = array(), $attributes = array())
   {
     $per = $this->getOption('per');
+
+    $cantcli = 0;
+    $cocliant='';
+    $cocli='';
+    foreach ($per as $p){
+      $cocli = $p['cocli'];
+      print $cocli;
+      if($cocliant!=$cocli){
+        $cantcli++;
+        $cocliant=$cocli;
+      }
+    }
+    
     $this->setWidgetSchema(new sfWidgetFormSchema(array(
       'resultado'  => new sfWidgetFormGrid(array('config' => $this->getOption('config'),'per' => $per)) ,
-      'cantidad_de_clientes'   => new sfWidgetFormInput(array(),array('readonly'=>true, 'value' => count($per))),
+      'cantidad_de_clientes'   => new sfWidgetFormInput(array(),array('readonly'=>true, 'value' => $cantcli)),
       'total_pagado'   => new sfWidgetFormInput(array(),array('readonly'=>true, 'value' => '', 'name' => 'totalp')),
       'mensaje' => new sfWidgetFormTextarea(array(),array('rows' => '3', 'cols' => '60')),
     )));
